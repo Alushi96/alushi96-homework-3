4cb8f3415bac4cd62a7e84665906e139711a970c
The Mechanic <jaber4132@gmail.com>
2020-02-22 22:53:03 -0500

    Step 2 - Update javascript with values

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var generateBtn = document.querySelector("#generate");
+
+var values = {
+    special: ["!@#$%^&*()"],
+    numeric: ["1234567890"],
+    lowercaseletters: ["abcdefghijklmnopqrstuvwxyz"],
+    uppercaseletters: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    none: [],
+}
